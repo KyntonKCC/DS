a4400d96cc1d13053c87816c0f43c96a60872e6e
Kaicheng <kyntonchuang2003@gmail.com>
2024-10-07 07:48:38 +0800

    finished hw1_p1_a without '('&')'

--- homework/hw1_p1_a.c
+++ homework/hw1_p1_a.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_STACK_SIZE 100
 typedef enum{
     lparen, rparen, plus, minus, times, divide, mod, eos, operand
@@ -16,9 +17,28 @@ char stackEmpty();
 int remtop = -1, sizetop = -1, length = 0;
 
 int main(void){
-    scanf("%s", expression);
-    for(int i = 0; expression[i] != '\0'; i++, length++);
-    Prefix_to_Infix();
+    FILE *fileinput, *fileoutput;
+    char fileinputname[] = "hw1_p1_a_input.txt";
+    char fileoutputname[] = "hw1_p1_a_output.txt";
+    fileinput = fopen(fileinputname, "r");
+    fileoutput = fopen(fileoutputname, "w");
+    if(fileinput == NULL || fileoutput == NULL){
+        printf("Fail to open file\n");
+        return 1;
+    }
+
+    while (fgets(expression, sizeof(expression), fileinput) != NULL) {
+        for(int i = 0; expression[i] != '\0'; i++, length++);
+        Prefix_to_Infix();
+        for(int i = remtop; i >= 0; i--)
+            fprintf(fileoutput, "%c", pop());
+        size[sizetop - 1] = 0;
+        sizetop = -1;
+        length = 0;
+    }
+
+    fclose(fileinput);
+    fclose(fileoutput);
     return 0;
 }
 
@@ -30,58 +50,58 @@ void Prefix_to_Infix(){
         if(token == operand){
             push(symbol);
             size[++sizetop] = 1;
-            printf("%c %d, remtop = %d, sizetop = %d\n", remember[remtop], size[sizetop], remtop, sizetop);
+            // printf("%c %d, remtop = %d, sizetop = %d\n", remember[remtop], size[sizetop], remtop, sizetop);
         }else{
             char op1[MAX_STACK_SIZE], op2[MAX_STACK_SIZE];
-            printf("size = %d op2 : ", size[sizetop]);
+            // printf("size = %d op2 : ", size[sizetop]);
             for(int i = 0; i < size[sizetop]; i++){
                 op2[i] = pop();
-                printf("%c ", op2[i]);
+                // printf("%c ", op2[i]);
             }
-            printf("\nsize = %d op1 : ", size[sizetop - 1]);
+            // printf("\n");
+            // printf("size = %d op1 : ", size[sizetop - 1]);
             for(int i = 0; i < size[sizetop - 1]; i++){
                 op1[i] = pop();
-                printf("%c ", op1[i]);
+                // printf("%c ", op1[i]);
             }
-            printf("\n");
-            push(')');
+            // printf("\n");
+            // push(')');
             for(int i = size[sizetop - 1] - 1; i >= 0; i--)
                 push(op1[i]);
             rememberToken(token);
             for(int i = size[sizetop] - 1; i >= 0; i--)
                 push(op2[i]);
-            push('(');
-            size[sizetop - 1] = size[sizetop] + size[sizetop - 1] + 3;
+            // push('(');
+            size[sizetop - 1] = size[sizetop] + size[sizetop - 1] + 1;
             size[sizetop] = 0;
             sizetop--;
         }
         token = getToken(&symbol, &n);
     }
-    for(int i = remtop; i >= 0; i--)
-        printf("%c", remember[i]);
-    printf("\n");
 }
+
 precedence getToken(char *symbol, int *n){
     *symbol = expression[(*n)--];
     switch(*symbol){
-        case '(': return lparen;
-        case ')': return rparen;
+        // case '(': return lparen;
+        // case ')': return rparen;
         case '+': return plus;
         case '-': return minus;
         case '/': return divide;
         case '*': return times;
-        case '%': return mod;
+        // case '%': return mod;
         case '\0': return eos;
         default : return operand;
     }
 }
+
 void rememberToken(precedence item){
     switch(item){
         case plus: push('+'); break;
         case minus: push('-'); break;
         case divide: push('/'); break;
         case times: push('*'); break;
-        case mod: push('%'); break;
+        // case mod: push('%'); break;
     }
 }
 
@@ -101,10 +121,10 @@ char pop(){
 
 void stackFull(){
     fprintf(stderr, "Stack is full, cannot add element\n");
-    // exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
 
 char stackEmpty(){
     fprintf(stderr, "Stack is empty, cannot pop element\n");
-    // exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
 }
